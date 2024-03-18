af5360123f493aa6cf1caa3521273a1b71aaa0bf
G2EJ-IC <IC.G2EJ@outlook.com>
2024-03-17 22:17:03 -0500

    Commit Inicial - 2024.03.17 22:16.
    Commit Inicial - 2024.03.17 22:16.

---
+++ Ej_EEZ_LVGLv8_3_11/Ej-EEZ_vButons/lib/ui/screens.h
@@ -0,0 +1,36 @@
+#ifndef EEZ_LVGL_UI_SCREENS_H
+#define EEZ_LVGL_UI_SCREENS_H
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _objects_t {
+    lv_obj_t *main;
+    lv_obj_t *bt_conectar_wi_fi;
+    lv_obj_t *bt_des_conectar_wi_fi;
+    lv_obj_t *panel_boton01;
+    lv_obj_t *panel_text01;
+    lv_obj_t *wi_fi_cautivo;
+} objects_t;
+
+extern objects_t objects;
+
+enum ScreensEnum {
+    SCREEN_ID_MAIN = 1,
+};
+
+void create_screen_main();
+void tick_screen_main();
+
+void create_screens();
+void tick_screen(int screen_index);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*EEZ_LVGL_UI_SCREENS_H*/
